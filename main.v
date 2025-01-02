84b41520b76d5566fe480e01e40ba80b51339e1e
archyboy <archy@workmail.com>
2025-01-02 19:11:47 +0100

    Update main.v

--- main.v
+++ main.v
@@ -1,4 +1,4 @@
-// V script for calculating randomness accuracy using PRNG randomness "rand.rand()" vs machine randomnesss crypto.rand()
+// V script for calculating randomness accuracy using PRNG randomness "rand.rand()" vs machine randomness crypto.rand()
 
 module main
 
